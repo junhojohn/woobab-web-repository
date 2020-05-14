794b492988a5d2681abfd28b33547e1e4d466dc5
junhojohn <junhojohn@gmail.com>
2020-05-14 21:56:50 +0900

    검색어 기반 지도 마커 표시 api 테스트 중.

--- WebProject01/src/test/java/com/test/woobab/controllers/TestKakaoMapAPIController.java
+++ WebProject01/src/test/java/com/test/woobab/controllers/TestKakaoMapAPIController.java
@@ -5,6 +5,7 @@ import org.apache.commons.logging.LogFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -16,11 +17,22 @@ public class TestKakaoMapAPIController {
 		log.info(getClass().getName() + "() called.");
 	}
 	
-	@RequestMapping(value="testKakaoMap.test", method=RequestMethod.GET)
-	public ModelAndView testKakaoMap() {
+	@RequestMapping(value="testMain.test", method=RequestMethod.GET)
+	public ModelAndView testMain() {
 		log.info(getClass().getName() + ".testKakaoMap() started.");
 		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.setViewName("testViews/testMain.jsp");
+		log.info(getClass().getName() + ".testKakaoMap() ended.");
+		return modelAndView;
+	}
+	
+	@RequestMapping(value="testKakaoMap.test", method=RequestMethod.POST)
+	public ModelAndView testKakaoMap(@RequestParam(value="location_search_keyword", required=true) String locationSearchKeyword) {
+		log.info(getClass().getName() + ".testKakaoMap() started.");
+		log.info("[PARAM01] location_search_keyword:" + locationSearchKeyword);
+		ModelAndView modelAndView = new ModelAndView();
 		modelAndView.setViewName("testViews/testKakaoMap.jsp");
+		modelAndView.addObject("location_search_keyword", locationSearchKeyword);
 		log.info(getClass().getName() + ".testKakaoMap() ended.");
 		return modelAndView;
 	}
