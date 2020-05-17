ff28ef12073f66d8f8c84a72fc721d5a387da2f1
junhojohn <junhojohn@gmail.com>
2020-05-17 22:27:54 +0900

    주소검색 화면 테스트 중

---
+++ WebProject01/src/test/java/com/test/woobab/controllers/TestSearchAddrAPIController.java
@@ -0,0 +1,27 @@
+package com.test.woobab.controllers;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class TestSearchAddrAPIController {
+
+	private Log log = LogFactory.getLog(TestSearchAddrAPIController.class);
+	
+	public TestSearchAddrAPIController() {
+		log.info(getClass().getName() + "() called.");
+	}
+	
+	@RequestMapping(value="/testSearchAddr.test", method=RequestMethod.GET)
+	public ModelAndView testSearchAddr() {
+		log.info(getClass().getName() + ".testSearchAddr() started.");
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.setViewName("testViews/testSearchAddr.jsp");
+		log.info(getClass().getName() + ".testSearchAddr() ended.");
+		return modelAndView;
+	}
+}
